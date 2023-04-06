1e3c61534841d02efc74026ecfc1b22fe6245b24
myself <1907312518@qq.com>
2023-04-06 22:57:56 +0800

    实现 Login 功能

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,73 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+
+const isShow = ref(true);
+const register = ref({
+  username: '',
+  password: '',
+  isError: false,
+  notice: '输入用户名和密码'
+});
+const login = ref({
+  username: '',
+  password: '',
+  isError: false,
+  notice: '创建账户后，请记住用户名和密码'
+});
+const onShowRegister = () => isShow.value = true;
+const onShowLogin = () => isShow.value = false;
+const validUsername = (username: string) => {
+  return {
+    isValid: /^[a-zA-Z_\d\u4E00-\u9FA5]{3,15}$/.test(username),
+    notice: '用户名必须是3~15个字符，限数字字母下划线中文'
+  };
+};
+const validPassword = (password: string) => {
+  return {
+    isValid: /^.{6,16}$/.test(password),
+    notice: '密码长度为6~16个字符'
+  };
+};
+const onRegister = () => {
+  //验证用户名
+  const result1 = validUsername(register.value.username);
+  if (!result1.isValid) {
+    register.value.isError = true;
+    register.value.notice = result1.notice;
+    return;
+  }
+  //验证密码
+  const result2 = validPassword(register.value.password);
+  if (!result2.isValid) {
+    register.value.isError = true;
+    register.value.notice = result2.notice;
+    return;
+  }
+  register.value.isError = false;
+  register.value.notice = '';
+  //TODO 提交注册信息
+  console.log('start register...,username:', register.value.username, 'password:', register.value.password);
+};
+const onLogin = () => {
+  //验证用户名
+  const result1 = validUsername(login.value.username);
+  if (!result1.isValid) {
+    login.value.isError = true;
+    login.value.notice = result1.notice;
+    return;
+  }
+  //验证密码
+  const result2 = validPassword(login.value.password);
+  if (!result2.isValid) {
+    login.value.isError = true;
+    login.value.notice = result2.notice;
+    return;
+  }
+  login.value.isError = false;
+  login.value.notice = '';
+  //TODO 提交注册信息
+  console.log('start login...,username:', login.value.username, 'password:', login.value.password);
+};
 </script>
 
 <template>
@@ -7,17 +76,19 @@
       <div class="login-container">
         <div class="main"/>
         <div class="form">
-          <h3>创建账户</h3>
-          <div v-show="false" class="register">
-            <input type="text" placeholder="用户名">
-            <input type="password" placeholder="密码">
-            <div class="button">创建账号</div>
+          <h3 @click="onShowRegister">创建账户</h3>
+          <div v-show="isShow" class="register">
+            <input v-model="register.username" type="text" placeholder="用户名">
+            <input v-model="register.password" type="password" placeholder="密码">
+            <p :class="{error:register.isError}">{{ register.notice }}</p>
+            <div @click="onRegister" class="button">创建账号</div>
           </div>
-          <h3>登录</h3>
-          <div v-show="true" class="login">
-            <input type="text" placeholder="用户名">
-            <input type="password" placeholder="密码">
-            <div class="button">登录</div>
+          <h3 @click="onShowLogin">登录</h3>
+          <div v-show="!isShow" class="login">
+            <input v-model="login.username" type="text" placeholder="用户名">
+            <input v-model="login.password" type="password" placeholder="密码">
+            <p :class="{error:login.isError}">{{ login.notice }}</p>
+            <div @click="onLogin" class="button">登录</div>
           </div>
         </div>
       </div>
@@ -91,6 +162,16 @@
 
             &:focus {
               border: 1px solid #9dcaf8;
+            }
+          }
+
+          p {
+            font-size: 12px;
+            margin-top: 1em;
+            color: #444;
+
+            &.error {
+              color: red;
             }
           }
 
