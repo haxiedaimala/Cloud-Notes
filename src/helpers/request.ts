e9966986b2e4765d3c48eb145d99b4f989e22436
myself <1907312518@qq.com>
2023-04-07 15:47:20 +0800

    引入 axios，封装统一接口

---
+++ src/helpers/request.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+type Options = {
+  url: string,
+  methods: string,
+  params?: {},
+  data?: {},
+}
+axios.defaults.baseURL = 'https://note-server.hunger-valley.com/';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+//跨域请求是否需要凭证(带上cookie)
+axios.defaults.withCredentials = true;
+
+export default function request(url: string, type = 'GET', data = {}) {
+  return new Promise((resolve, reject) => {
+    const option: Options = {
+      url,
+      methods: type
+    };
+    if (type.toLowerCase() === 'get') {
+      option.params = data;
+    } else {
+      option.data = data;
+    }
+    axios(option).then(result => {
+      if (result.status === 200) {
+        resolve(result.data);
+      } else {
+        console.error(result.data);
+        reject(result.data);
+      }
+    }).catch(() => {
+      console.error({msg: '网络异常'});
+      reject({msg: '网络异常'});
+    });
+  });
+}
