0e200434cf4ca9777622b0b652c0540c21f02176
myself <1907312518@qq.com>
2023-04-08 09:25:25 +0800

    状态码400，处理错误原因的解决方法二：axios 配置状态码分类

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -5,6 +5,7 @@ type Options = {
   method: string,
   params?: {},
   data?: {},
+  validateStatus: (status: number) => boolean
 }
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
@@ -15,24 +16,23 @@ export default function request(url: string, type = 'GET', data = {}) {
   return new Promise((resolve, reject) => {
     const option: Options = {
       url,
-      method: type
+      method: type,
+      //当状态码不符合区间时，返回false会reject直接进入catch
+      validateStatus: (status) => (status >= 200 && status < 300) || status === 400,
     };
     if (type.toLowerCase() === 'get') {
       option.params = data;
     } else {
       option.data = data;
     }
-    axios(option).then(
-      result => {
-        if (result.status === 200) {
-          resolve(result.data);
-        }
-      },
-      error => {
-        console.error(error.response.data);
-        reject(error.response.data);
+    axios(option).then(result => {
+      if (result.status === 200) {
+        resolve(result.data);
+      } else {
+        console.error(result.data);
+        reject(result.data);
       }
-    ).catch(() => {
+    }).catch(() => {
       console.error({msg: '网络异常'});
       reject({msg: '网络异常'});
     });
