cd34c82e91c349b43aee50226af8781c053ca2c7
myself <1907312518@qq.com>
2023-04-08 15:34:37 +0800

    未登录时 跳转登录界面

--- src/views/NoteDetail.vue
+++ src/views/NoteDetail.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+import Auth from '../api/auth';
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+Auth.getInfo().then(data => {
+  let result = data as UserInfo;
+  if (!result.isLogin) {
+    router.push({path: '/login'});
+  }
+});
 </script>
 
 <template>
--- src/views/TrashDetail.vue
+++ src/views/TrashDetail.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import Auth from '../api/auth';
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+Auth.getInfo().then(data => {
+  let result = data as UserInfo;
+  if (!result.isLogin) {
+    router.push({path: '/login'});
+  }
+});
 </script>
 
 <template>
   <div>TrashDetail.vue</div>
-  <h1>{{$route.params.noteId}}</h1>
+  <h1>{{ $route.params.noteId }}</h1>
 </template>
 
 <style lang="scss" scoped>
